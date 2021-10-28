9dc0cd34ec4f108286761c8078910cbd1f4bc759
Angela Bosilkovska <angela.bosilkovska@uklo.edu.mk>
2021-10-28 22:07:49 +0200

    init

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Welcome to the best corny dad jokes on the web</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Dad Jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes",
+        },
+      ],
+    };
+  },
+};
+</script>
