255528738ad8695b5c41956555313b683af9d488
Angela Bosilkovska <angela.bosilkovska@uklo.edu.mk>
2021-11-01 08:32:44 +0100

    Adding Header

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -3,7 +3,33 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      jokes: [],
+    };
+  },
+  async created() {
+    const config = {
+      headers: {
+        Accept: "applicaton/json",
+      },
+    };
+    const res = await axios.get();
+  },
+  head() {
+    return {
+      title: "Dad Jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style></style>
